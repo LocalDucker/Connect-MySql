7124e96f737a4ac5cd1589a64044a804d4473f61
Oleksii Dziuma <alkaiiah909@gmail.com>
2019-10-22 21:50:02 +0200

    v1.0

---
+++ DBUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace Sql.Connection
+{
+    class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "localhost";
+            int port = 3306;
+            string database = "lab";
+            string username = "root";
+            string password = "mysql";
+
+            return DBMySqlUtils.GetDBConnection(host, port, database, username, password);
+        }
+    }
+}
